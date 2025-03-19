82f4f387b89ecc1e5e8d2d5b846d8222234eab05
MohannadBT <xsmile3x@gmail.com>
2025-03-20 02:23:18 +0300

    Back to to it with section 18

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,32 @@
+import styles from "./CountryList.module.css";
+import Spinner from "./Spinner";
+import Message from "./Message";
+import CountryItem from "./CountryItem";
+import { useCities } from "../contexts/CitiesContext";
+
+function CountryList() {
+  const { cities, isLoading } = useCities();
+
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="add your first city by Clicking on a city on the map" />
+    );
+
+  const countries = cities.reduce((acc, cur) => {
+    if (!acc.map((el) => el.country).includes(cur.country))
+      return [...acc, { country: cur.country, emoji: cur.emoji, id: cur.id }];
+    else return acc;
+  }, []);
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.id} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
