f06d0985ef7ed1aa281e1b8ab5c821ec2fa4fbfa
MohannadBT <xsmile3x@gmail.com>
2025-03-23 00:12:57 +0300

    End of section 18

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Login.module.css";
 import PageNav from "../components/PageNav";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/FakeAuthContext";
+import Button from "../components/Button";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
 
+  const navigate = useNavigate();
+  const { isAuthenticated, login } = useAuth();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if ((email, password)) login(email, password);
+  }
+
+  useEffect(
+    function () {
+      if (isAuthenticated) navigate("/app", { replace: true });
+    },
+    [isAuthenticated]
+  );
+
   return (
     <main className={styles.login}>
       <PageNav />
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
@@ -33,9 +50,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button>
-            <Link to="/app">Login</Link>
-          </button>
+          <Button type="primary">Login</Button>
         </div>
       </form>
     </main>
